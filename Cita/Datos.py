bbacc0b23d085c8dbec9cb7212f0edb44dc89a06
araceliimarcoos <araceliimarcoos@gmail.com>
2026-02-26 20:13:27 -0800

    #Delete de clases

--- Cita/Datos.py
+++
@@ -1,7 +0,0 @@
-from Cita import Cita
-citas = [
-    Cita(1, "14/01/2026", "08:30", "Entrevista", "Hecha"),
-    Cita(2, "13/01/2026", "12:30", "Entrevista", "Hecha"),
-    Cita(3, "18/02/2026", "13:30", "Doctor", "Pendiente"),
-    Cita(4, "14/01/2026", "09:30", "Entrevista", "Hecha"),
-    Cita(5, "20/01/2026", "10:30", "Reunion", "Pendiente")]
--- Pedidos/DPlatillos.py
+++
@@ -1,8 +0,0 @@
-'''(self, platilloID=0, nombre="", precio=0, disponibilidad="Disponible"):'''
-from Platillo import Platillo
-Platillos=[
-    Platillo(1,"Rollo Mar y Tierra", 120, "Disponible"),
-    Platillo(2,"Teriyaqui", 150, "Disponible"),
-    Platillo(3,"Ramen de Pollo", 130, "Disponible"),
-    Platillo(4,"Ramen con Camaron", 145, "No Disponible"),
-]
